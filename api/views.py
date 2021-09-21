642a3f3ae5f09b71eafa3ddb7cc46fa423dd0c57
Alexsander <farispamfull@gmail.com>
2021-09-21 04:21:56 +0300

    fix and add EmailConfirmationView

--- api/views.py
+++ api/views.py
@@ -1,18 +1,22 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
-from .serializer import SignUpSerializer
+from .serializer import SignUpSerializer, TokenSerializer
 from .utils import send_token_for_user
 
 
 class SignUpApiView(APIView):
     permission_classes = [AllowAny]
+    serializer_class = SignUpSerializer
 
     def post(self, request):
-        serializer = SignUpSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         email = serializer.validated_data['email']
         username = serializer.validated_data['username']
@@ -21,5 +25,27 @@ class SignUpApiView(APIView):
             is_active=False
         )
         send_token_for_user(request, user)
+
         return Response({'email': email}, status=status.HTTP_200_OK)
-   
+
+
+class EmailConfirmation(APIView):
+    serializer_class = TokenSerializer
+
+    def get_token(self, user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
+    def post(self, request):
+        serializer = TokenSerializer(request.data)
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(User, email=serializer.data['email'])
+        confirmation_code = serializer.data['confirmation_code']
+        if default_token_generator.check_token(user, confirmation_code):
+            user.is_active = True
+            user.save()
+            token = self.get_token(user)
+            Response({'token': token}, status=status.HTTP_200_OK)
+
+        response = {'confirmation_code': 'Invalid confirmation code'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
