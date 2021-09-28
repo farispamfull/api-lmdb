f795cfed4414d4038d6bb10e7837018b1487a597
Alexsander <farispamfull@gmail.com>
2021-09-29 00:39:46 +0300

    docker comprose config

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -5,10 +5,8 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('DB_ENGINE')
+DEBUG = os.getenv('DEBUG', 'True')
 
 ALLOWED_HOSTS = ['*']
 
